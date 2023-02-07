99e915f5c8412360d0e22f62051c8e8758341ace
sixtusagbo <miracleagbosixtus@gmail.com>
2023-02-07 18:43:52 +0100

    [Update] Case parent is NULL

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -13,7 +13,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node = binary_tree_node(parent, value);
 
-	if ((parent == NULL) || (node == NULL))
+	if (parent == NULL)
 		return (NULL);
 
 	if (parent->left != NULL)
